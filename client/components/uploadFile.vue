c65265515add92f13bdcb4b3c8983cbceb3f8371
Alonso Avila <avila@zaa.mx>
2017-01-16 01:52:50 -0600

    ya funciona dinamicamente

--- client/components/uploadFile.vue
+++ client/components/uploadFile.vue
@@ -1,20 +1,13 @@
 <template>
   <div>
     <div class="upload-compita card card-outline-primary">
-      <DropVue
-        id="imagen"
-
-        ref="imagen"
-          :newUrl="urlSuccessPost"
-
-        :useCustomDropzoneOptions="true"
-        :dropzoneOptions="opcionesx"
-        v-on:vdropzone-fileAdded="fileAddedImage"
-        v-on:vdropzone-success="successImage"
-        v-on:vdropzone-error="errorImage"
-        v-on:vdropzone-removedFile="removedFileImage"
-        v-on:vdropzone-sending="sendingImage"
-        ></DropVue>
+      <form class="dropzone">
+      <!-- Not displayed, just for Dropzone's `dictDefaultMessage` option -->
+      <div id="dropzone-message" style="display: none">
+        <span class="dropzone-title">Drop files here or click to select</span>
+        <span class="dropzone-info">You can upload multiple files at once</span>
+      </div>
+    </form>
     </div>
     <hr>
     <button class="btn btn-outline-info" @click="sendImage">
@@ -24,46 +17,68 @@
 </template>
 
 <script>
-import DropVue from 'components/DropVue'
+// import DropVue from 'components/DropVue'
+import Dropzone from 'dropzone'
+Dropzone.autoDiscover = false
 
 export default {
   data () {
     return {
-      test: 'lorem',
-      urlSuccessPost: '587c5bae7e7ab52e565eca1a',
-      opcionesx: {
-        url: 'http://hispanojobs.stamplayapp.com/api/cobject/v1/jobs',
-        init: function() {
-          var vm = this
-          this.on('processing', function(file) {
-            console.log('test',vm)
-            var url =  'http://hispanojobs.stamplayapp.com/api/cobject/v1/jobs/' + vm.urlSuccessPost;
-            this.url = url
-            console.log('la url de envio', this.url)
-          });
-        },
-        method: "PUT",
-        paramName: "imagen", // The name that will be used to transfer the file
-        maxFilesize: 1,
-        uploadMultiple: false,
-        acceptedFiles: 'image/*',
-        maxFiles: 1,
-        // thumbnailWidth:300,
-        addRemoveLinks:true,
-        headers: {
-          'Cache-Control': null,
-          'X-Requested-With': null
-        },
-        autoProcessQueue: false,
-        dictDefaultMessage: 'sube cosas rata'
-      }
+      id: 'lorem',
+      urlSuccessPost: '587c6bf3d8aa408b1480bdbf'
     }
   },
-  computed : {
-    cosas () {
-      console.log('esto llego', this.urlSuccessPost)
-      return  'http://hispanojobs.stamplayapp.com/api/cobject/v1/jobs/' + this.urlSuccessPost
+  mounted () {
+    const vm = this
+    let options = {
+      // The URL will be changed for each new file being processing
+      url: '/',
+      // Since we're going to do a `PUT` upload to S3 directly
+      method: 'patch',
+      // Hijack the xhr.send since Dropzone always upload file by using formData
+      // ref: https://github.com/danialfarid/ng-file-upload/issues/743
+      // sending (file, xhr) {
+      //   let _send = xhr.send
+      //   xhr.send = () => {
+      //     _send.call(xhr, file)
+      //   }
+      // },
+      paramName: "imagen",
+      // Upload one file at a time since we're using the S3 pre-signed URL scenario
+      parallelUploads: 1,
+      uploadMultiple: false,
+      // Content-Type should be included, otherwise you'll get a signature
+      // mismatch error from S3. We're going to update this for each file.
+      headers: {
+        'Cache-Control': null,
+        'X-Requested-With': null
+      },
+      // Customize the wording
+      dictDefaultMessage: document.querySelector('#dropzone-message').innerHTML,
+      // We're going to process each file manually (see `accept` below)
+      autoProcessQueue: false,
+      // Here we request a signed upload URL when a file being accepted
+      // accept (file, done) {
+      //   lambda.getSignedURL(file)
+      //     .then((url) => {
+      //       file.uploadURL = url
+      //       done()
+      //       // Manually process each file
+      //       setTimeout(() => vm.dropzone.processFile(file))
+      //     })
+      //     .catch((err) => {
+      //       done('Failed to get an S3 signed upload URL', err)
+      //     })
+      // }
     }
+    // Instantiate Dropzone
+    this.dropzone = new Dropzone(this.$el, options)
+    // Set signed upload URL for each file
+    vm.dropzone.on('processing', (file) => {
+      vm.dropzone.options.url = 'http://hispanojobs.stamplayapp.com/api/cobject/v1/jobs/' + vm.urlSuccessPost
+    })
+  },
+  computed : {
   },
   methods: {
     showSuccess (file, response) {
@@ -87,16 +102,18 @@ export default {
       console.log('data enviando -->', formData)
     },
     sendImage() {
-      this.$refs.imagen.processQueue()
+
+      this.dropzone.processQueue()
       console.log('click en el boton')
     }
   },
   components: {
-    DropVue
+
   }
 
 }
 </script>
 
 <style lang="scss">
+@import '../../node_modules/dropzone/dist/dropzone.css'
 </style>
