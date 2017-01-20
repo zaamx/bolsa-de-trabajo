63ea8dca0a102cb34f8223a948bec654cd490605
Alonso Avila <aavila@hispanocard.com>
2017-01-19 18:46:35 -0600

    logo

--- client/components/App.vue
+++ client/components/App.vue
@@ -4,7 +4,10 @@
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="#">HispanoClub Jobs</a>
+      <!-- <a class="navbar-brand" href="#">HispanoClub Jobs</a> -->
+      <a class="navbar-brand" href="#">
+        <img src="../assets/logo-sitio.png" alt="">
+      </a>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
           <router-link class="nav-item nav-link" to="/" active-class="active">
